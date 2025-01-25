6f59bba716e0159d217c35df5bff378a1dfa1edc
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2025-01-25 23:44:01 +0700

    minor update in component
    legit just one condition to display if find description in variable
    whatis this i mean technically before it apply to GStore
    it do have information....hmmmmmmm

--- frontend/src/components/horizontal-component.vue
+++ frontend/src/components/horizontal-component.vue
@@ -2,7 +2,7 @@
   <div class="horizontal-container">
     <div v-for="item in events" :key="item.id">
       <div>{{ item.title }}</div>
-      <div>
+      <div v-if="item.description">
         <a @click.prevent="navigateToDetail(item)" class="event-link">
           click here for more detail
         </a>
